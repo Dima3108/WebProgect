027d3181c28570f64b6bb8f8626fc86d54bfd59a
Dima <73338242+Dima3108@users.noreply.github.com>
2023-01-12 21:40:41 +0300

    addreact

---
+++ WebApplicationProgect/wwwroot/js/form_react.jsx
@@ -0,0 +1,48 @@
+﻿
+class MyLableName extends React.Component {
+    render() {
+        return <label>Ваше имя:</label>
+    }
+}
+class MyInputName extends React.Component {
+    render() {
+        return <input type="text" name="your_name" />
+    }
+}
+class MyDIVName extends React.Component {
+    render() {
+        return <div>
+            <MyLableName />
+            <MyInputName/>
+            </div>
+    }
+}
+class MyLableMail extends React.Component {
+    render() {
+        return <label>Ваш email:</label>
+    }
+}
+class MyInputMail extends React.Component {
+    render() {
+        return <input type="email" name="your_email" />
+    }
+}
+class MyDIVMail extends React.Component {
+    render() {
+        return <div>
+            <MyLableMail />
+            <MyInputMail/>
+            </div>
+    }
+}
+class MyForm extends React.Component {
+    render() {
+        return <form >
+            <div className="form-group">
+            <MyDIVName />
+                <MyDIVMail />
+                </div>
+            </form>
+    }
+}
+ReactDOM.render(<MyForm/>, document.getElementById('react_content'));
