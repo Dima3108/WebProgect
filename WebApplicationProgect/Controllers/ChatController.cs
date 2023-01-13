d3316d1b1c8eb078314bc054beb43aa626533959
Dima <73338242+Dima3108@users.noreply.github.com>
2023-01-13 21:48:01 +0300

    w

---
+++ WebApplicationProgect/Controllers/ChatController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationProgect.Controllers
+{
+    public class ChatController : Controller
+    {
+        [HttpGet]
+        public int ReadMessage(string user,string message)
+        {
+            Console.WriteLine($"user:{user},comment:{message}");
+            return 0;
+        }
+    }
+}
